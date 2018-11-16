920001930c85efb12f792aadb79190b437b9a934
Kaylem-Lee Andrews <42963595+KaylemLee17@users.noreply.github.com>
2018-11-16 14:02:28 +0200

    finished structure and background of project-3

---
+++ inc/quiz.php
@@ -0,0 +1,48 @@
+<?php
+    $answers1 = $_POST['question-1-answers'];
+
+    $answers2 = $_POST['question-2-answers'];
+
+    $answers3 = $_POST['question-3-answers'];
+
+    $answers4 = $_POST['question-4-answers'];
+
+    $answers5 = $_POST['question-5-answers'];
+
+    $answers6 = $_POST['question-6-answers'];
+
+    $answers7 = $_POST['question-7-answers'];
+
+    $answers8 = $_POST['question-8-answers'];
+
+    $answers9 = $_POST['question-9-answers'];
+
+    $answers10 = $_POST['question-10-answers'];
+
+    $answers11 = $_POST['question-11-answers'];
+
+    $answers12 = $_POST['question-12-answers'];
+
+    $answers13 = $_POST['question-13-answers'];
+
+    $answers14 = $_POST['question-14-answers'];
+
+    $answers15 = $_POST['question-15-answers'];
+
+    $answers16 = $_POST['question-16-answers'];
+
+    $answers17 = $_POST['question-17-answers'];
+
+    $answers18 = $_POST['question-18-answers'];
+
+    $answers19 = $_POST['question-19-answers'];
+
+    $answers20 = $_POST['question-20-answers'];
+
+    $totalCorrect = 0;
+
+    foreach ($answer as $answer) {
+        $answers1 ==
+    }
+
+?>
